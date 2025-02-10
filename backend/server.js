fca5cf78233a24316672ae880a1f7a9c08eb7097
Yulian Khai <yulik19971997@gmail.com>
2025-02-10 19:28:03 +0200

    setup backedn part 2: Basic Express server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server....')
+const express = require('express')
+const doteenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.status(200).json({message: 'Welcome to the Support Desk API'})
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
