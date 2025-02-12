0ae76ce97c80be345dc20e7ef8560330c895fec0
Yulian Khai <yulik19971997@gmail.com>
2025-02-12 18:38:37 +0200

    Initials route and pages, add component Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router'
+import { Home as HomePage } from './pages/Home'
+import { Login as LoginPage } from './pages/Login'
+import { Register as RegisterPage } from './pages/Register'
+import { Header } from './components/Header'
+
 function App() {
 
-  return (<div>My app</div>)
+  	return <>
+		<Router>
+			<div className="container">
+				<Header />
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/login' element={<LoginPage />} />
+					<Route path='/register' element={<RegisterPage />} />
+				</Routes>
+			</div>
+		</Router>
+	</>
 }
 
 export default App
