e3b029e6bba256e408e0d543c10c08567a9ced3f
Yulian Khai <yulik19971997@gmail.com>
2025-02-14 16:40:10 +0200

    Get Tickets, Create Tickets

---
+++ backend/controllers/ticketController.js
@@ -0,0 +1,22 @@
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+const Ticket = require('../models/ticketModel')
+
+// @desc	Get user tickets
+// @route	GET /api/tickets
+// @access	Private
+const getTickets = asyncHandler(async(req, res) => {
+	res.status(200).json({message: 'get Tickets'})
+})
+
+// @desc	Create new ticket
+// @route	POST /api/tickets
+// @access	Private
+const createTicket = asyncHandler(async(req, res) => {
+	res.status(200).json({message: 'create Ticket'})
+})
+
+module.exports = {
+	getTickets,
+	createTicket
+}
