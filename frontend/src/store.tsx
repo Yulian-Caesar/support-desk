0d619788e161de25447f03a7e0aa639f1360f3b5
Yulian Khai <yulik19971997@gmail.com>
2025-02-15 14:44:59 +0200

    fetch notes through Redux

--- frontend/src/store.tsx
+++ frontend/src/store.tsx
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice'
 import ticketReducer from './features/tickets/ticketSlice'
+import noteReducer from './features/notes/noteSlice'
 import { Provider } from 'react-redux';
 import { ReactNode } from 'react';
 
@@ -8,7 +9,8 @@ import { ReactNode } from 'react';
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
-		tickets: ticketReducer
+		tickets: ticketReducer,
+		notes: noteReducer
 	},
 });
 
