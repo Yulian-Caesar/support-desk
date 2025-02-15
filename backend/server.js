32f1b11f9200c1595a02a1d4edd49047e5be80b3
Yulian Khai <yulik19971997@gmail.com>
2025-02-15 16:31:05 +0200

    deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const colors = require('colors')
 const doteenv = require('dotenv').config()
 const PORT = process.env.PORT || 5173;
@@ -18,13 +19,21 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-	res.status(200).json({message: 'Welcome to the Support Desk API'})
-})
-
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve frontend
+if(process.env.NODE_ENV === 'production') {
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+	app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'dist', 'index.html'))
+} else {
+	app.get('/', (req, res) => {
+		res.status(200).json({message: 'Welcome to the Support Desk API'})
+	})
+}
 
 app.use(errorHandler)
 
